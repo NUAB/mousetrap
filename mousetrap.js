76dfc81fce5860c4a8a8bfa7759d7fbbd8f44c52
Marcus Rosell <marcusrosell@deadbyapril.com>
2021-02-10 18:33:38 +0100

    added method for isChrome var

--- mousetrap.js
+++ mousetrap.js
@@ -195,19 +195,9 @@
 
         const isNumPadOrDigit = (e.code.includes('Digit') || e.code.includes('Numpad'))
 
-        if(isChrome && !Mousetrap.getIsRecording() && e.type == 'keydown'){ // chrome does not trigger keypress when ctrl is pushed down
-        var character = isNumPadOrDigit ? e.code : String.fromCharCode(e.which);
-
-            if (!e.shiftKey) {
-                character = character.toLowerCase();
-            }
-
-            return character;
-        }
-
         // for keypress events we should return the character as is
         if ( e.type == 'keypress') {
-            var character = isNumPadOrDigit ? e.code : String.fromCharCode(e.which);
+            var character = isNumPadOrDigit ? e.code.toLowerCase() : String.fromCharCode(e.which);
 
             // if the shift key is not pressed then it is safe to assume
             // that we want the character to be lowercase.  this means if
@@ -225,6 +215,12 @@
             return character;
         }
 
+        if(Mousetrap.getIsChrome() && !Mousetrap.getIsRecording() && e.type == 'keydown'){ // chrome does not trigger keypress when ctrl is pushed down
+            var character = isNumPadOrDigit ? e.code.toLowerCase() : String.fromCharCode(e.which);
+    
+                return character;
+            }
+
         // for non keypress events the special maps are needed
         if (_MAP[e.which]) {
             return _MAP[e.which];
@@ -239,7 +235,7 @@
         // with keydown and keyup events the character seems to always
         // come in as an uppercase character whether you are pressing shift
         // or not.  we should make sure it is always lowercase for comparisons
-        return isNumPadOrDigit ? e.code : String.fromCharCode(e.which).toLowerCase();
+        return isNumPadOrDigit ? e.code.toLowerCase() : String.fromCharCode(e.which).toLowerCase();
     }
 
     /**
@@ -1056,6 +1052,10 @@
 
     Mousetrap.prototype.getIsRecording = () => {
         return mouseTrapIsRecording;
+    }
+
+    Mousetrap.prototype.getIsChrome = () => {
+        return isChrome;
     }
 
     Mousetrap.prototype.setIsRecording = (bool) => {
